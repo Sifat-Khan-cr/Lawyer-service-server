db9cce970b7211c107c71752650e87c5d156dd68
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-11-08 19:38:22 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 app.use(cors());
 app.use(express.json());
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.get('/', (req, res) => {
@@ -13,3 +15,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dzhzyda.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+
+        const Service = client.db('lawyer').collection('services');
+        // Service.insertOne({ name: 'lawer' })
+    } catch (error) {
+        console.error(error);
+    }
+}
+run();
